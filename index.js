d2f61aed11197581d05df1bd3c75e23b2893420b
“Guada” <guadalupearroferia@gmail.com>
2024-02-26 14:50:07 +1000

    added delete function

--- index.js
+++ index.js
@@ -1,86 +1,123 @@
+
 const cart=document.getElementById("cartList");
 const totalPrice=document.getElementById("total");
-const prodName=document.getElementsByClassName("product_name");
-const prodPrice=document.getElementsByClassName("card__price");
-const addBtn=document.getElementsByClassName("card__btn");
+const cardTarget=document.getElementById("cardContainer");
+const cartTarget=document.getElementById("cartList");
 
-class productInfo{
-    constructor(name, price, img, id){
-     this.name=name;
-     this.price=price;
-     this.img=img;
-     this.id=id;
-    } 
-}
 
-const product1= new productInfo(
-    "Pancetta y hongos",
-    "6262",
-    "./img/Pizza1.png",
-    "btn-1"
-)
 
-const product2= new productInfo(
-    "Jamon y huevo",
-    "4069",
-    "./img/Pizza2.png",
-    "btn-2"
-)
+const ourPizzas=[
+    {
+        id: 1,
+        flavor:"Pancetta y hongos",
+        price: "6262",
+        img:"./img/Pizza1.png",
+        btn:"btn-1"
 
-const product3= new productInfo(
-    "Margarita",
-    "2499",
-    "./img/Pizza3.png",
-    "btn-3"
-)
+    },
+    {
+        id: 2,
+        flavor:"Jamon y huevo",
+        price: "4069",
+        img:"./img/Pizza2.png",
+        btn:"btn-2"
 
-const product4= new productInfo(
-    "Jamon y anana",
-    "4299",
-    "./img/Pizza4.png",
-    "btn-4"
-)
+    },
+    {
+        id: 3,
+        flavor:"Margarita",
+        price: "2499",
+        img:"./img/Pizza3.png",
+        btn:"btn-3"
 
-const product5= new productInfo(
-    "Langostinos y salsa golf",
-    "6079",
-    "./img/Pizza5.png",
-    "btn-5"
-)
+    },
+    {
+        id: 4,
+        flavor:"Jamon y anana",
+        price: "4299",
+        img:"./img/Pizza4.png",
+        btn:"btn-4"
 
-const product6= new productInfo(
-    "Pollo y espinaca",
-    "4395",
-    "./img/Pizza6.png",
-    "btn-6"
-)
+    },
+    {
+        id: 5,
+        flavor:"Langostinos y salsa golf",
+        price: "6532",
+        img:"./img/Pizza5.png",
+        btn:"btn-5"
+
+    },
+    {
+        id: 6,
+        flavor:"Pollo y espinaca",
+        price: "4395",
+        img:"./img/Pizza6.png",
+        btn:"btn-6"
+
+    }
+];
 
 function createCards(){
-    return `<div class="card">
+for (const pizza of ourPizzas) {
+    cardTarget.innerHTML += `<div class="card" id=card-${pizza.id}>
     <div class="img_container">
-        <img src="${this.img}" alt="" class="card_img">
+        <img src="${pizza.img}" alt="" class="card_img">
     </div>
     <div class="card_text">
-        <p class="card__price">$${this.price}</p>
-        <h3 class="card__h3 product_name">${this.name}</h3>
-        <button class="card__btn" id="${this.id}">Agregar</button>
+        <p class="card__price pizzaPrice">$${pizza.price}</p>
+        <h3 class="card__h3 product_name pizzaFlavor">${pizza.flavor}</h3>
+        <button class="card__btn" id="${pizza.btn}" onclick="savePizza('${pizza.flavor}', '${pizza.price}')">Agregar</button>
     </div>
 </div>`
 }
+}
+
+document.addEventListener("DOMContentLoaded", createCards);
+
+
+var addedPizzas;
 
 function createCart(){
-    return `
-    <p class="cart_name">${this.name}</p>
-    <p class="cart_price" id="cartPrice">${this.price}</p>
-    `
+    if(localStorage.getItem('AddedPizzas')){
+        cartTarget.innerHTML ="";
+        addedPizzas= JSON.parse(localStorage.getItem('AddedPizzas'));
+        for (let i = 0; i < addedPizzas.length; i++) {
+            let pizza= addedPizzas[i]  
+            cartTarget.innerHTML +=`<div class= "eachPizza">
+            <p class="cart_name cart_flavour">${pizza.flavor}</p>
+            <p class="cart_price cart_price">$ ${pizza.price}</p>
+            <button class="cart_btn" onClick="deletePizza(${i})">x</button>
+            </div>
+            `
+        }
+    }
+}
+
+function savePizza(flavor, price){
+    const pizza = {
+        flavor: flavor,
+        price: price,
+    };
+    if (localStorage.getItem('AddedPizzas')) {
+        addedPizzas = JSON.parse(localStorage.getItem('AddedPizzas'));
+    } else {
+        addedPizzas = [];
+    }
+    addedPizzas.push(pizza);
+    localStorage.setItem('AddedPizzas', JSON.stringify(addedPizzas));
+    
+    alert("Pizza agregada");
+    createCart();
+}
+
+const cartBtn = document.getElementsByClassName("cart_btn")
+
+function deletePizza(index){
+    addedPizzas= JSON.parse(localStorage.getItem('AddedPizzas'));
+    addedPizzas.splice(index, 1);
+    localStorage.setItem('AddedPizzas', JSON.stringify(addedPizzas));
+    createCart();
 }
 
 
 
-
-
-
-const totalPriceDiv=document.getElementById("totalPrice");
-function totalPrice(){
-
-}
